bd85d6a5cf30755797bec6bfcea34df998515c47
Issah Akuffo <issahkuffo300@gmail.com>
2025-05-17 19:45:18 +0000

    update

---
+++ controllers/contacts.js
@@ -0,0 +1,26 @@
+ const mongodb = require('../data/database');
+ const ObjectId = require('mongodb').ObjectId;
+ 
+ // Get all users
+ const getAll = async (req, res) => {
+     const result = await mongodb.getDatabase().collection('contacts').find();
+     result.toArray().then((contacts) => {
+         res.setHeader('Content-Type', 'application/json');
+         res.status(200).json(contacts);
+     });
+ };
+ 
+ // Get a single contact by ID
+ const getSingle = async (req, res) => {
+     const contactId = new ObjectId(req.params.id);
+     const result = await mongodb.getDatabase().collection('contacts').find({ _id:contactId });
+     result.toArray().then((contacts) => {
+         res.setHeader('Content-Type', 'application/json');
+         res.status(200).json(contacts[0]);
+     });
+ };
+ 
+ module.exports = {
+     getAll,
+     getSingle
+ };
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const cors = require('cors');
+//const cors = require('cors');
 const mongodb = require('./data/database');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+//app.use(cors());
 app.use(express.json());
 
 app.use(express.static('public'));
