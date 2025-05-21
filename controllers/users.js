6860ba20549d574a527eae303f9a792c0ee17360
Issah Akuffo <issahkuffo300@gmail.com>
2025-05-21 02:06:10 +0000

    week 2 assigment update

--- controllers/users.js
+++ controllers/users.js
@@ -3,6 +3,7 @@ const ObjectId = require('mongodb').ObjectId;
 
 // Get all users
 const getAll = async (req, res) => {
+    //#swagger.tags=['users']
     const result = await mongodb.getDatabase().collection('users').find();
     result.toArray().then((users) => {
         res.setHeader('Content-Type', 'application/json');
@@ -12,6 +13,7 @@ const getAll = async (req, res) => {
 
 // Get a single user by ID
 const getSingle = async (req, res) => {
+    //#swagger.tags=['users']
     const userId = new ObjectId(req.params.id);
     const result = await mongodb.getDatabase().collection('users').find({ _id: userId });
     result.toArray().then((users) => {
@@ -20,7 +22,55 @@ const getSingle = async (req, res) => {
     });
 };
 
+const createUser = async (req,res) =>{
+    //#swagger.tags=['users']
+    const user = {
+        frstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColour: req.body.favoriteColour,
+        birthday: req.body.birthday
+    };
+    const response = await mongodb.getDatabase().collection('users').inserteOne(user);
+    if (response.acknowledge){
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occured while updating the user.')
+    }
+};
+
+const updateUser = async (req,res) =>{
+    //#swagger.tags=['users']
+    const userId = new ObjectId(req.params.id);
+    const user = {
+        username: req.body.username,
+        email: req.body.email,
+        name: req.body.name,
+        ipaddress: req.body.ipaddress
+    };
+    const response = await mongodb.getDatabase().collection('users').replaceOne({ _id: userId }, user);
+    if (response.modifiedCount > 0){
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occured while updating the user.')
+    }
+};
+
+
+const deleteUser = async (req,res) =>{
+    //#swagger.tags=['users']
+    const userId = new ObjectId(req.params.id);
+    const response = await mongodb.getDatabase().collection('users').deleteOne({ _id: userId });
+    if (response.deletedCount > 0){
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occured while deleting the user.')
+    }
+};
 module.exports = {
     getAll,
-    getSingle
+    getSingle,
+    createUser,
+    updateUser,
+    deleteUser
 };
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 //const cors = require('cors');
+const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
 
 const app = express();
@@ -11,6 +12,16 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.use('/', require('./routes'));
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTION');
+    next();
+});
 
 mongodb.initDb((err) => {
     if (err) {
